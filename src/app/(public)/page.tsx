e4c66bb1ea91e91856a1e1e8d5e40a9bc5593b54
Luis Otávio Pilotto <luis_tavio@hotmail.com>
2025-04-26 02:30:54 -0300

    feat: Atualizando sistema de login

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -4,8 +4,16 @@ import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import HowToWorks from "@/components/HowToWorks";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-const LandignPage = () => {
+const LandignPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/dashboard");
+  }
   return (
     <main id="inicio" className="flex flex-col min-h-screen">
       {/* Header Section */}
